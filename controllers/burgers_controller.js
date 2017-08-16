a728cf22a18015d954076a553b192f76a2f5b151
Vincent Fiorilli <Vincent Fiorilli>
2017-08-16 11:34:51 -0400

    create route working

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -9,12 +9,6 @@ router.get("/", function(req, res) {
 });
 
 router.get("/burgers", function(req, res) {
-  // express callback response by calling burger.selectAllBurger
-  // db.Burger.findAll(function(data) {
-  //   // Wrapping the array of returned burgers in a object so it can be referenced inside our handlebars
-  //   var hbsObject = { burgers: data };
-  //   res.render("index", hbsObject);
-  // });
   db.Burger.findAll({})
   	.then(function(dbPost){
   		var hbsObject = { burgers: dbPost };
@@ -22,25 +16,28 @@ router.get("/burgers", function(req, res) {
   	})
 });
 
-// // post route -> back to index
-// router.post("/burgers/create", function(req, res) {
-//   // takes the request object using it as input for buger.addBurger
-//   burger.create(req.body.burger_name, function(result) {
-//     // wrapper for orm.js that using MySQL insert callback will return a log to console,
-//     // render back to index with handle
-//     console.log(result);
-//     res.redirect("/");
-//   });
-// });
+// post route -> back to index
+router.post("/burgers/create", function(req, res) {
+	db.Burger.create({
+  	burger_name: req.body.burger_name,
+  	devoured: false
+  }).then(function() {
+  	// var hbsObject = { burgers: dbPost };
+   //  res.render("index", hbsObject);
+   res.redirect("/burgers")
+  })
+});
 
-// // put route -> back to index
-// router.put("/burgers/update", function(req, res) {
-//   burger.update(req.body.burger_id, function(result) {
-//     // wrapper for orm.js that using MySQL update callback will return a log to console,
-//     // render back to index with handle
-//     console.log(result);
-//     res.redirect("/");
-//   });
-// });
+// put route -> back to index
+router.put("/burgers/update", function(req, res) {
+  // burger.update(req.body.burger_id, function(result) {
+  //   // wrapper for orm.js that using MySQL update callback will return a log to console,
+  //   // render back to index with handle
+  //   console.log(result);
+  //   res.redirect("/");
+  // });
+
+ 
+});
 
 module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -5,10 +5,13 @@
 
 module.exports = function(sequelize, DataTypes) {
   var Burger = sequelize.define("Burger", {
-    burger_name: DataTypes.STRING,
+    burger_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
     devoured: {
       type: DataTypes.BOOLEAN,
-      defaultValue: false
+      defaultValue: false,
     },
     date: {
       type: DataTypes.DATE,
